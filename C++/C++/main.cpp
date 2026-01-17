0c155cf0a596a80d1a7dad3dc7d99d008741d386
Richmond <Richlaureta@gmail.com>
2026-01-16 21:23:40 -0800

    Setting up C++ Beyond Cracking the Coding Interview.

---
+++ C++/C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by Richmond Laureta on 1/16/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
