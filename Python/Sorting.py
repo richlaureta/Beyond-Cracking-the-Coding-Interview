ff9e105027d5dafd3b4633917ad9ee13b4bd7854
Richmond <Richlaureta@gmail.com>
2026-06-30 21:06:10 -0700

    Added an incomplete solution to Problem #31.2 - Nested Circles in Python. I will complete the solution.

--- Python/Sorting.py
+++ Python/Sorting.py
@@ -22,6 +22,49 @@ def letterOccurrences(word: str) -> list[str]:
     
     return sortedLetterByFrequencyList
 
+def areCirclesNested(circles: list[tuple]) -> bool:
+    #Problem 31.2 - Nested Circles
+    
+    currentMaxX = float('-inf')
+    currentMaxXIndex = None
+    currentMinimumX = float('inf')
+    currentMinimumXIndex = None
+    
+    currentMaxY = float('-inf')
+    currentMaxYIndex = None
+    currentMinimumY = float('inf')
+    currentMinimumYIndex = None
+    
+    currentMaxRadiusHigherX = None
+    currentMaxRadiusHigherY = None
+    currentMaxRadiusLowerX = None
+    currentMaxRadiusLowerY = None
+    currentMaxRadiusIndex = None
+    
+    currentMaxRadius = float('-inf')
+    
+    
+    for index in range(len(circles)):
+        if currentMaxRadius < abs(circles[index][1]):
+            currentMaxRadius = abs(circles[index][1])
+            currentMaxRadiusIndex = index
+            currentMaxRadiusHigherX = circles[index][0][0] + abs(circles[index][1])
+            currentMaxRadiusHigherY = circles[index][0][1] + abs(circles[index][1])
+            currentMaxRadiusLowerX = circles[index][0][0] - abs(circles[index][1])
+            currentMaxRadiusLowerY = circles[index][0][1] - abs(circles[index][1])
+        
+        if circles[index][0][0] + abs(circles[index][1]):
+            currentMaxX = circles[index][0][0] + abs(circles[index][1])
+            currentMaxXIndex = index
+            
+        if currentMinimumX >= circles[index][0][0] - abs(circles[index][1]):
+            currentMinimumX = circles[index][0][0] - abs(circles[index][1])
+            currentMinimumXIndex = index
+        
+        if currentMaxY <= circles[index][0][1] + abs(circles[index][1]):
+            currentMaxY = circles[index][0][1] + abs(circles[index][1])
+            currentMaxYIndex = index
+        
 #TESTS
 
 def letter_occurrences_lambda(word):
@@ -83,4 +126,9 @@ def RunAllSortingTests():
     print("------------------------------------------------")
   
 if __name__ == "__main__":
-    RunAllSortingTests()
+    circles = [
+        ((4, 4), 5),
+        ((8, 4), 2)
+    ]
+    
+    print(areCirclesNested(circles))
