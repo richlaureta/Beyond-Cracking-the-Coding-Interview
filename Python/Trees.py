98b590f45df3e0eb6abf59a5fc68dfef9131b1ce
Richmond <Richlaureta@gmail.com>
2026-09-24 11:04:33 -0700

    Added a solution to Problem 35.10 - Zig-Zag Order in Python.

--- Python/Trees.py
+++ Python/Trees.py
@@ -260,6 +260,49 @@ def most_prolific_level(root: Node):
         max_prolific_level = popped_node[1] - 1
             
     return max_prolific_level
+
+def zig_zag_order(root: Node):
+    #Problem 35.10 - Zig-zag Order
+    
+    if not root:
+        return []
+    
+    zigzag_list = [root.val]
+    depth_level_set = set([-1, 0])
+    
+    level_value_list_dictionary = defaultdict(list)
+    node_level_deueue = deque([(root, 0)])
+    
+    popped_node_level = None
+    while node_level_deueue:
+        popped_node_level = node_level_deueue.popleft()
+        level_value_list_dictionary[popped_node_level[1]].append(popped_node_level[0].val)
+        
+        if popped_node_level[1] - 1 not in depth_level_set:
+            if (popped_node_level[1] - 1) % 2 == 0:
+                for number in level_value_list_dictionary[popped_node_level[1] - 1]:
+                    zigzag_list.append(number)
+            else:
+                for number in reversed(level_value_list_dictionary[popped_node_level[1] - 1]):
+                    zigzag_list.append(number)
+            
+            depth_level_set.add(popped_node_level[1] - 1)
+                    
+        if popped_node_level[0].left:
+            node_level_deueue.append((popped_node_level[0].left, popped_node_level[1] + 1))
+            
+        if popped_node_level[0].right:
+            node_level_deueue.append((popped_node_level[0].right, popped_node_level[1] + 1))
+    
+    if popped_node_level[1] not in depth_level_set:
+            if popped_node_level[1] % 2 == 0:
+                for number in level_value_list_dictionary[popped_node_level[1]]:
+                    zigzag_list.append(number)
+            else:
+                for number in reversed(level_value_list_dictionary[popped_node_level[1]]):
+                    zigzag_list.append(number)
+            
+    return zigzag_list
         
 #TESTS
 
@@ -691,7 +734,65 @@ def run_most_prolific_level_tests():
         i + 1}): got: {got}, valid_wants: {valid_wants}\n"
   
   print("ALL MOST PROLIFIC LEVEL TESTS PROVIDED HAVE PASSED.")
-  
+
+def run_zigzag_order_tests():
+  # Example 1 from the book
+  root1 = Node(1)
+  root1.left = Node(2)
+  root1.right = Node(3)
+  root1.left.left = Node(4)
+  root1.left.right = Node(5)
+  root1.right.left = Node(6)
+  root1.right.right = Node(7)
+
+  # Example 2 - empty tree
+  root2 = None
+
+  # Example 3 - single node
+  root3 = Node(1)
+
+  # Example 4 - unbalanced tree
+  root4 = Node(1)
+  root4.left = Node(2)
+  root4.left.left = Node(3)
+  root4.left.left.left = Node(4)
+
+  # Example 5 - complete binary tree
+  root5 = Node(1)
+  root5.left = Node(2)
+  root5.right = Node(3)
+  root5.left.left = Node(4)
+  root5.left.right = Node(5)
+  root5.right.left = Node(6)
+  root5.right.right = Node(7)
+  root5.left.left.left = Node(8)
+  root5.left.left.right = Node(9)
+  root5.left.right.left = Node(10)
+  root5.left.right.right = Node(11)
+  root5.right.left.left = Node(12)
+  root5.right.right.left = Node(14)
+  root5.right.right.right = Node(15)
+
+  tests = [
+      # Example 1 - basic tree
+      (root1, [1, 3, 2, 4, 5, 6, 7]),
+      # Example 2 - empty tree
+      (root2, []),
+      # Example 3 - single node
+      (root3, [1]),
+      # Example 4 - unbalanced tree
+      (root4, [1, 2, 3, 4]),
+      # Example 5 - complete binary tree
+      (root5, [1, 3, 2, 4, 5, 6, 7, 15, 14, 12, 11, 10, 9, 8])
+  ]
+
+  for i, (root, want) in enumerate(tests):
+    got = zig_zag_order(root)
+    assert got == want, f"\nExample {
+        i + 1}: zig_zag_order({root}): got: {got}, want: {want}\n"
+
+  print("ALL ZIG-ZAG ORDER TESTS PROVIDED HAVE PASSED.")
+
 #ALL TESTS
 
 def Run_All_Trees_Tests():
@@ -703,6 +804,7 @@ def Run_All_Trees_Tests():
     run_evaluate_expression_tree_tests()
     run_left_view_tests()
     run_most_prolific_level_tests()
+    run_zigzag_order_tests()
     
     print()
     print("--------------------------------------------------")
@@ -710,4 +812,4 @@ def Run_All_Trees_Tests():
     print("--------------------------------------------------")
     
 if __name__ == "__main__":
-    Run_All_Trees_Tests()
+    run_zigzag_order_tests()
